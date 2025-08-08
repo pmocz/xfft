45b8f1954a3309b7d49036b3e847483d8f848e1a
pmocz <philip.mocz@gmail.com>
2025-08-08 11:05:01 -0400

    update figs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -23,14 +23,17 @@ sbatch --time=0-00:10 sbatch_rusty.sh 64 double
 sbatch --time=0-00:10 sbatch_rusty.sh 128 double
 sbatch --time=0-00:10 sbatch_rusty.sh 256 double
 sbatch --time=0-00:10 sbatch_rusty.sh 512 double
+sbatch --time=0-00:10 sbatch_rusty.sh 1024 double
 
 sbatch --time=0-00:10 --nodes=1 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 64 double
 sbatch --time=0-00:10 --nodes=1 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 128 double
 sbatch --time=0-00:10 --nodes=1 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 256 double
 sbatch --time=0-00:10 --nodes=1 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 512 double
+sbatch --time=0-00:10 --nodes=1 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 1024 double
 
 sbatch --time=0-00:10 --nodes=2 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 64 double
 sbatch --time=0-00:10 --nodes=2 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 128 double
 sbatch --time=0-00:10 --nodes=2 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 256 double
 sbatch --time=0-00:10 --nodes=2 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 512 double
 sbatch --time=0-00:10 --nodes=2 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 1024 double
+sbatch --time=0-00:10 --nodes=2 --ntasks-per-node=4 --gpus-per-node=4 --cpus-per-task=1  sbatch_rusty.sh 2048 double
